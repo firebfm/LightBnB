652b8f45e109a1a9f3f620a9acde0af760f24cca
Simon Huang <firebfm@gmail.com>
2021-01-07 23:39:08 +0000

    add function getAllReservations

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -86,7 +86,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  return pool.query(queryString, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(err => console.error('ERROR AT SHOWING RESERVATIONS', err));
 }
 exports.getAllReservations = getAllReservations;
 
